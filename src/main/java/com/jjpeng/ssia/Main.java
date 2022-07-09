4859e24aeebbb383d1d5ac826e24b8b58671fbb6
JJPeng <pengjiajun755@163.com>
2022-07-09 23:57:46 +0800

    创建项目，并创建HelloController并暴露hello rest接口，观察到访问hello接口时会被重定向到login页面

---
+++ src/main/java/com/jjpeng/ssia/Main.java
@@ -0,0 +1,16 @@
+package com.jjpeng.ssia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JJPeng
+ * @date 2022/7/9 23:10
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
