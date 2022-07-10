61a1fd17f7666e0e62200f15ea5217de8142614e
JJPeng <pengjiajun755@163.com>
2022-07-10 15:20:41 +0800

    添加用户信息，并创建一个密码编码器（此编码器不会对密码进行加密）

--- src/main/java/com/jjpeng/ssia/config/ProjectConfig.java
+++ src/main/java/com/jjpeng/ssia/config/ProjectConfig.java
@@ -2,7 +2,11 @@ package com.jjpeng.ssia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
@@ -15,6 +19,17 @@ public class ProjectConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails userDetails = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(userDetails);
         return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //此编码器不会对密码进行加密，依然使用明文
+        return NoOpPasswordEncoder.getInstance();
     }
 }
