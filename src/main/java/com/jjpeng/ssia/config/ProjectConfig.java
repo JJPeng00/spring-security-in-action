cb34a51677de1edf3c1d3ee4e5d38e3479ac4de9
JJPeng <pengjiajun755@163.com>
2022-07-10 16:14:02 +0800

    修改覆盖UserDetailsService和PasswordEncoder的方式，之前是通过向spring 上下文中注入bean的方式覆盖配置 现在改用重写WebSecurityConfigureAdapter中的configure的方式覆盖默认配置

--- src/main/java/com/jjpeng/ssia/config/ProjectConfig.java
+++ src/main/java/com/jjpeng/ssia/config/ProjectConfig.java
@@ -1,14 +1,12 @@
 package com.jjpeng.ssia.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
@@ -18,28 +16,23 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
         UserDetails userDetails = User.withUsername("john")
                 .password("12345")
                 .authorities("read")
                 .build();
         userDetailsService.createUser(userDetails);
-        return userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        //此编码器不会对密码进行加密，依然使用明文
-        return NoOpPasswordEncoder.getInstance();
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.authorizeRequests()
-                //所有的请求都不需要认证
-                .anyRequest().permitAll();
+                //所有的请求都需要认证，和默认的配置一样
+                .anyRequest().authenticated();
     }
 }
