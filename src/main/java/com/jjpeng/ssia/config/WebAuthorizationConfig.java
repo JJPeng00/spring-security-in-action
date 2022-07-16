a5fa8dd509fc88242e7ec353415d1225bb27cc72
JJPeng <pengjiajun755@163.com>
2022-07-16 22:45:50 +0800

    第六章，实践、A small secured web application： 配置注入（使用）自定义的AuthenticationProvider

--- src/main/java/com/jjpeng/ssia/config/WebAuthorizationConfig.java
+++ src/main/java/com/jjpeng/ssia/config/WebAuthorizationConfig.java
@@ -1,7 +1,9 @@
 package com.jjpeng.ssia.config;
 
+import com.jjpeng.ssia.service.AuthenticationProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -11,11 +13,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationProviderService authenticationProvider;
+
+    //认证是由AuthenticationManager调用AuthenticationProvider的方法完成的，所以当我们需要指定provider时，
+    //应进行如下配置（使用参数为AuthenticationManagerBuilder的configure方法
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider);
     }
 }
