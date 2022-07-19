c8b6f40e28bb6dae3702635f74c87f81aa60dcd9
JJPeng <pengjiajun755@163.com>
2022-07-19 23:02:32 +0800

    第九章，自定义日志过滤器，并添加到原认证过滤器的位置（不是替换，是不在项目中引入BasicAuthenticationFilter） 验证： curl -H "Authorization:SD9cICjl1e" localhost:8080/hello

--- src/main/java/com/jjpeng/ssia/config/WebAuthorizationConfig.java
+++ src/main/java/com/jjpeng/ssia/config/WebAuthorizationConfig.java
@@ -1,7 +1,7 @@
 package com.jjpeng.ssia.config;
 
-import com.jjpeng.ssia.filter.AuthenticationLoggingFilter;
-import com.jjpeng.ssia.filter.RequestValidationFilter;
+import com.jjpeng.ssia.filter.StaticKeyAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,20 +14,20 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @Configuration
 public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private StaticKeyAuthenticationFilter staticKeyAuthenticationFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
+        //注释掉httpBasic是为了BasicAuthenticationFilter的实例不会被添加到过滤链中
+//        http.httpBasic();
         //关闭CSRF 为了可以调用/a路径的post请求，不关闭的话会401
         http.csrf().disable();
 
-        //将自定义过滤器添加到认证过滤器之前
-        http.addFilterBefore(
-                        new RequestValidationFilter(),
-                        BasicAuthenticationFilter.class)
-                //在认证过滤器之后添加日志过滤器
-                .addFilterAfter(
-                        new AuthenticationLoggingFilter(),
-                        BasicAuthenticationFilter.class);
+        //将自定义的认证过滤器添加在原BasicAuthenticationFilter的位置
+        // 因为http.httpBasic()被注释掉了，所以不会引入BasicAuthenticationFilter的实例，所以这并不等同于替换
+        // 因为同一个位置允许通常存在多个过滤器，但是这些过滤器的过滤顺序是不确定的，所以应该避免在同一个位置添加多个过滤器
+        http.addFilterAt(staticKeyAuthenticationFilter, BasicAuthenticationFilter.class);
 
         http.authorizeRequests()
                 .anyRequest().permitAll();
