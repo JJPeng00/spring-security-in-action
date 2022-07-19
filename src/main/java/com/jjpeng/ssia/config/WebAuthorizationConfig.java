8da24894fbb75747988257abe129f79ffd0eded9
JJPeng <pengjiajun755@163.com>
2022-07-19 23:31:51 +0800

    第九章，拓展Spring Security提供的抽象类实现更强大的功能 验证： curl -H "Request-Id:12345" http://localhost:8080/hello

--- src/main/java/com/jjpeng/ssia/config/WebAuthorizationConfig.java
+++ src/main/java/com/jjpeng/ssia/config/WebAuthorizationConfig.java
@@ -1,5 +1,6 @@
 package com.jjpeng.ssia.config;
 
+import com.jjpeng.ssia.filter.AuthenticationLoggingFilter;
 import com.jjpeng.ssia.filter.StaticKeyAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -19,15 +20,13 @@ public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //注释掉httpBasic是为了BasicAuthenticationFilter的实例不会被添加到过滤链中
-//        http.httpBasic();
+        http.httpBasic();
         //关闭CSRF 为了可以调用/a路径的post请求，不关闭的话会401
         http.csrf().disable();
 
-        //将自定义的认证过滤器添加在原BasicAuthenticationFilter的位置
-        // 因为http.httpBasic()被注释掉了，所以不会引入BasicAuthenticationFilter的实例，所以这并不等同于替换
-        // 因为同一个位置允许通常存在多个过滤器，但是这些过滤器的过滤顺序是不确定的，所以应该避免在同一个位置添加多个过滤器
-        http.addFilterAt(staticKeyAuthenticationFilter, BasicAuthenticationFilter.class);
+        http.addFilterAfter(
+                new AuthenticationLoggingFilter(),
+                BasicAuthenticationFilter.class);
 
         http.authorizeRequests()
                 .anyRequest().permitAll();
--- src/main/java/com/jjpeng/ssia/filter/AuthenticationLoggingFilter.java
+++ src/main/java/com/jjpeng/ssia/filter/AuthenticationLoggingFilter.java
@@ -1,25 +1,34 @@
 package com.jjpeng.ssia.filter;
 
 
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.logging.Logger;
 
 /**
  * @author JJPeng
  * @date 2022/7/19 21:58
+ *
+ * Spring Security提供了一些实现了Filter接口的抽象类
+ * 当我们需要实现某一功能是，优先考虑是否可以通过拓展Spring Security提供的抽象类来是实现功能
+ *
+ * 因为Spring Security并不能保证每一个过滤器不被多次调用，所以我们需要在过滤器中自己实现，OncePerRequestFilter是Spring Security实现的抽象类，该类保证此过滤器在一次请求中只被调用一次，我们通过拓展该类实现我们的功能
+ *
+ * 还可以通过重写OncePerRequestFilter中的其他方法实现其他实用的功能
  */
-public class AuthenticationLoggingFilter implements Filter {
+public class AuthenticationLoggingFilter extends OncePerRequestFilter {
 
     private final Logger logger = Logger.getLogger(AuthenticationLoggingFilter.class.getName());
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         String requestId = httpServletRequest.getHeader("Request-Id");
 
         logger.info("Successfully authenticated request with id " + requestId);
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
