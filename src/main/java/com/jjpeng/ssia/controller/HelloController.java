303827390e773ae17480087605de48f237b85a7a
JJPeng <pengjiajun755@163.com>
2022-07-27 23:04:16 +0800

    第十六章：global method security preauthorization for authorities，方法级别的预验证 为方法的参数定义授权规则，本例中，只有方法的参数与认证用户的用户名相等时才能调用方法
    验证：
    1. 方法参数与登录用户名一致，授权成功
    curl -u emma:12345 http://localhost:8080/secret/names/emma
    返回：
    ["Fantastico"]
    2. 登录用户名与方法参数不一致，授权失败
    curl -u emma:12345 http://localhost:8080/secret/names/natalie
    返回：{"timestamp":"2022-07-27T14:59:08.404+00:00","status":403,"error":"Forbidden","message":"","path":"/secret/names/natalie"}

--- src/main/java/com/jjpeng/ssia/controller/HelloController.java
+++ src/main/java/com/jjpeng/ssia/controller/HelloController.java
@@ -3,8 +3,11 @@ package com.jjpeng.ssia.controller;
 import com.jjpeng.ssia.service.NameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author JJPeng
@@ -19,6 +22,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String getHello() {
         return "hello " + nameService.getName();
+    }
+
+    @GetMapping("/secret/names/{name}")
+    public List<String> names(@PathVariable String name) {
+        return nameService.getSecretNames(name);
     }
 
     @PostMapping("/hello")
--- src/main/java/com/jjpeng/ssia/service/NameService.java
+++ src/main/java/com/jjpeng/ssia/service/NameService.java
@@ -3,12 +3,26 @@ package com.jjpeng.ssia.service;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author JJPeng
  * @date 2022/7/27 22:37
  */
 @Service
 public class NameService {
+
+    Map<String, List<String>> map = Map.of(
+            "natalie", List.of("Energico", "Perfecto"),
+            "emma", List.of("Fantastico")
+    );
+
+    //方法的参数必须等于当前认证用户的用户名才能调用此方法
+    @PreAuthorize("#name == authentication.principal.username")
+    public List<String> getSecretNames(String name) {
+        return map.get(name);
+    }
 
     //拥有写权限的用户才能调用此方法
     @PreAuthorize("hasAuthority('write')")
